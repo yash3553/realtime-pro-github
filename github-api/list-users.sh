d1763bc09271d00359fabd41f69b3122afa41b92
Babapeer <69247426+Babapeer@users.noreply.github.com>
2024-07-02 10:11:47 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#################################
+About: "this script will show the output details who are admins"
+Input: Username:Babapeer
+       Password:xxxxxxxx
+ #
+ #Owner: Abhishek Veeramalla
+ #
+ ##############################
+  helper()
+  
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -34,6 +44,13 @@ function list_users_with_read_access {
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
     fi
+}
+function helper
+{
+expected_cmd_args=2
+if[$# -ne $expected_cmd_args]; then
+echo "Please execute the script with required cmd args"
+echo "asd"
 }
 
 # Main script
